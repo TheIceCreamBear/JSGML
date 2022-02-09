865d607ddcb0ff1a6d5a57e700feeceb167e2dee
Joseph Terribile <kartingman25@gmail.com>
2022-02-09 14:33:04 -0500

    Add index

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import Vector2 from './vec/vec2';
+import Vector3 from './vec/vec3';
+import Vector4 from './vec/vec4';
+
+const JSGML_VERSION = 1.0;
+
+export { JSGML_VERSION, Vector2, Vector3, Vector4 };

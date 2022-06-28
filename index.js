37598fc9172649ca37760eecd56f52ecd1555797
Joseph Terribile <kartingman25@gmail.com>
2022-06-28 16:57:41 -0400

    Remove weird old number version forom JS

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ import Vector4 from './vec/vec4.js';
 import Matrix2 from './mat/mat2.js';
 import Matrix3 from './mat/mat3.js';
 
-const JSGML_VERSION = 1.0;
-
 // TODO: globally isFinite, equals function
 // TODO: matrix scaleLocal
 
-export { JSGML_VERSION, Vector2, Vector3, Vector4, Matrix2, Matrix3 };
+export { Vector2, Vector3, Vector4, Matrix2, Matrix3 };
